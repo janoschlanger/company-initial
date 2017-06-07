ab97bb31aa59afb7819c6e46a6003887d30af3c5
Janosch Langer <janosch.langer@cgi.com>
2017-06-07 10:57:16 -0600

    -

--- src/main/java/io/rscale/training/company/CompanyApplication.java
+++ src/main/java/io/rscale/training/company/CompanyApplication.java
@@ -2,10 +2,11 @@ package io.rscale.training.company;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class CompanyApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(CompanyApplication.class, args);
 	}
